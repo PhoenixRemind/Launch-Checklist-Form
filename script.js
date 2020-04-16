5bc8177c13a95d23454c6b82efc696be1733b088
Paul <pdeyarmin@gmail.com>
2020-04-16 15:10:58 -0400

    updated some basic styling

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ window.addEventListener("load", function() {
       selectList.appendChild(option);
    }
 
-   let planetSelect = mySelect.selectedIndex;
-
    selector.addEventListener('change', function() {
       missionData.innerHTML = `
       <h2>Mission Destination</h2>
@@ -58,18 +56,18 @@ window.addEventListener("load", function() {
 
       if(fuelLevelInput.value < 10000 && cargoMassInput.value <= 9999) {
          launchStatus.innerHTML = "<span style='color: red;'>Shuttle not ready for launch</span>";
-         fuelStatus.innerHTML = `Not enough fuel to complete journey`;
+         fuelStatus.innerHTML = `<span style='color: red;'>Not enough fuel to complete journey</span>`;
          cargoStatus.innerHTML = `Cargo mass low enough for launch`;
       }
       else if(cargoMassInput.value > 10000 && fuelLevelInput.value >= 10000) {
          launchStatus.innerHTML = "<span style='color: red;'>Shuttle not ready for launch</span>";
          fuelStatus.innerHTML = `Fuel level high enough for launch`;
-         cargoStatus.innerHTML = `There is too much mass for shuttle takeoff`;
+         cargoStatus.innerHTML = `<span style='color: red;'>There is too much mass for shuttle takeoff</span>`;
       }
       else if(cargoMassInput.value > 10000 && fuelLevelInput.value < 10000) {
          launchStatus.innerHTML = "<span style='color: red;'>Shuttle not ready for launch</span>";
-         fuelStatus.innerHTML = `Not enough fuel to complete journey`;
-         cargoStatus.innerHTML = `There is too much mass for shuttle takeoff`;
+         fuelStatus.innerHTML = `<span style='color: red;'>Not enough fuel to complete journey</span>`;
+         cargoStatus.innerHTML = `<span style='color: red;'>There is too much mass for shuttle takeoff</span>`;
       } else {
          launchStatus.innerHTML = "<span style='color: green;'>Shuttle is ready for launch!</span>";
          fuelStatus.innerHTML = `Fuel level high enough for launch`;
@@ -78,15 +76,3 @@ window.addEventListener("load", function() {
       });
    });
 });
-
-
-// This block of code shows how to format the HTML once you fetch some planetary JSON!
-// <h2>Mission Destination</h2>
-// <ol>
-//    <li>Name: ${}</li>
-//    <li>Diameter: ${}</li>
-//    <li>Star: ${}</li>
-//    <li>Distance from Earth: ${}</li>
-//    <li>Number of Moons: ${}</li>
-// </ol>
-// <img src="${}">
