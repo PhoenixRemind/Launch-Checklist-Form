f498004827dc5b23d86168fc19b9959837e57e7a
Paul <pdeyarmin@gmail.com>
2020-04-22 09:21:25 -0400

    corrected code indent

--- script.js
+++ script.js
@@ -3,41 +3,41 @@ window.addEventListener("load", function() {
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
         return(response.json());
     }).then(function (planetData) {
-   let form = document.querySelector("form");
-   let launchStatus = document.getElementById("launchStatus");
-   let pilotStatus = document.getElementById("pilotStatus");
-   let copilotStatus = document.getElementById("copilotStatus");
-   let fuelStatus = document.getElementById("fuelStatus");
-   let cargoStatus = document.getElementById("cargoStatus");
-   let faultyItems = document.getElementById("faultyItems");
-   const missionData = document.getElementById("missionTarget");
-   let dropdownValues = [planetData[0], planetData[1], planetData[2], planetData[3], planetData[4], planetData[5]];
-   let selectButton = document.getElementById("selector");
-   let selectList = document.createElement("select");
-   let numberInput = /[0-9]/;
-   selectList.setAttribute("id", "mySelect");
-   selectButton.appendChild(selectList);
+      let form = document.querySelector("form");
+      let launchStatus = document.getElementById("launchStatus");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let faultyItems = document.getElementById("faultyItems");
+      const missionData = document.getElementById("missionTarget");
+      let dropdownValues = [planetData[0], planetData[1], planetData[2], planetData[3], planetData[4], planetData[5]];
+      let selectButton = document.getElementById("selector");
+      let selectList = document.createElement("select");
+      let numberInput = /[0-9]/;
+      selectList.setAttribute("id", "mySelect");
+      selectButton.appendChild(selectList);
 
-   for (let i=0; i<dropdownValues.length; i++) {
-      let option = document.createElement("option");
-      option.setAttribute("value", dropdownValues[i]);
-      option.text = dropdownValues[i].name;
-      selectList.appendChild(option);
-   }
+      for (let i=0; i<dropdownValues.length; i++) {
+         let option = document.createElement("option");
+         option.setAttribute("value", dropdownValues[i]);
+         option.text = dropdownValues[i].name;
+         selectList.appendChild(option);
+      }
 
-   selector.addEventListener('click', function() {
-      missionData.innerHTML = `
-      <h2>Mission Destination</h2>
-      <ol>
-         <li>Name: ${planetData[mySelect.selectedIndex].name}</li>
-         <li>Diameter: ${planetData[mySelect.selectedIndex].diameter}</li>
-         <li>Star: ${planetData[mySelect.selectedIndex].star}</li>
-         <li>Distance from Earth: ${planetData[mySelect.selectedIndex].distance}</li>
-         <li>Number of Moons: ${planetData[mySelect.selectedIndex].moons}</li>
-      </ol>
-      <img src="${planetData[mySelect.selectedIndex].image}">
-      `;
-   });
+      selector.addEventListener('click', function() {
+         missionData.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${planetData[mySelect.selectedIndex].name}</li>
+            <li>Diameter: ${planetData[mySelect.selectedIndex].diameter}</li>
+            <li>Star: ${planetData[mySelect.selectedIndex].star}</li>
+            <li>Distance from Earth: ${planetData[mySelect.selectedIndex].distance}</li>
+            <li>Number of Moons: ${planetData[mySelect.selectedIndex].moons}</li>
+         </ol>
+         <img src="${planetData[mySelect.selectedIndex].image}">
+         `;
+      });
 
    form.addEventListener("submit", function(event) {
       let pilotNameInput = document.querySelector("input[name=pilotName]");
